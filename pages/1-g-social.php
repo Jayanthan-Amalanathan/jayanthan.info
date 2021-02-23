b28ff72227dc9ed6c5b389920143ade2498be16e
DESKTOP-O6AH0ED\ajaya <ajayanthan97@gmail.com>
2021-02-23 14:57:48 +0530

    Add social page

---
+++ pages/1-g-social.php
@@ -0,0 +1,58 @@
+<!--Education-->
+    <section id="social" class="resume">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Social</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6">
+            <h3 class="resume-title">Research</h3>
+            <div class="resume-item">
+              <h4>ICCC</h4>
+              <h5>2018</h5>
+              <p>“Optimizing the Effective use of Traditional Methods Over the Current Practice of Waste Management and its impact on Climate Change ” was an Oral Presentation Presented in the 2nd International Climate Change Conference 2018 organized by TIIKM with the Collaboration of Colorado State University and University of Colombo.</p>
+              <a href="https://climatechangeconferences.com/" target="_blank">climatechangeconferences.com/</a>
+            </div>
+            <div class="resume-item">
+              <h4>SJWP</h4>
+              <h5>2014</h5>
+              <p>“Portable Water Purification Method With Minimum Cost And Maximum Efficiency” is a research presented in the regional Stockholm Junior Water Prize (SJWP) - 2013.</p>
+              <a href="https://www.siwi.org/prizes/stockholmjuniorwaterprize/" target="_blank">stockholmjuniorwaterprize</a>
+            </div>
+            <h3 class="resume-title">Certification</h3>
+            <div class="resume-item">
+              <h4>Startup Business Management</h4>
+              <h5>2019</h5>
+              <p>Startup Business Management is a 6 month professional certification course of Startup Academy and Rising Tide Foundation.</p>
+              <a href="https://www.bookbridgelanka.com/bookings-checkout/startup-business-management-course-1" target="_blank">bookbridgelanka.com/startup-business-management-course-1</a>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <h3 class="resume-title">Academic</h3>
+            <div class="resume-item">
+              <h4>Undergraduate</h4>
+              <h5>2019 - Present</h5>
+              <p>Following bachelor of technology in Science and Technology at Uva Wellasa University, Sri Lanka.</p>
+              <a href="http://www.uwu.ac.lk/" target="_blank">Uva Wellassa University of Sri Lanka</a>
+            </div>
+            <div class="resume-item">
+              <h4>High school education</h4>
+              <h5>2013 - 2017</h5>
+              <p>Followed Bio Science course for Advanced level in Shivananda National School</p>
+              <a href="https://shivanandavidyalaya.org/" target="_blank">Bt/Shivananda National School</a>
+            </div>
+            <div class="resume-item">
+              <h4>Primary and Secondary Education</h4>
+              <h5>2003 - 2013</h5>
+              <p>Studied from Grade 1 to ordinary level in Shivananda National School.</p>
+              <a href="https://shivanandavidyalaya.org/" target="_blank">Bt/Shivananda National School</a>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+<!---End Education Section----->
